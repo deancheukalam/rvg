575430474a463c06c3781928954478afd53cb4ca
Dean Cheuk A Lam <dean.cheukalam@leanup-it.com>
2022-05-18 18:43:41 -0300

    Clean Old Themes that wont be used anymore

--- wp-content/themes/eGamer/includes/js.php
+++
@@ -1,3 +0,0 @@
-﻿<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/scrollTo.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/serialScroll.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/init.js"></script>
--- wp-content/themes/remyvastgoed_2021_bak4OKT2021/searchform.php
+++
@@ -1,8 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-    <div>
-    
-        <input type="text" id="s" name="s" value="" placeholder="Straatnaam of objectcode..." />
-        
-        <input type="submit" value="Search" id="searchsubmit" />
-    </div>
-</form>
