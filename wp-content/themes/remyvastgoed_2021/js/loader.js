6dade613aeb9f45ccdc7c745d1e4a2396b8c5390
Dean Cheuk A Lam <dean.cheukalam@leanup-it.com>
2022-05-19 16:49:30 -0300

    Fix Tooltips Met hoofdletter and Fix 404 page without plugin

--- wp-content/themes/remyvastgoed_2021/js/loader.js
+++ wp-content/themes/remyvastgoed_2021/js/loader.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
     //Removing tooltip for pages and adding tooltips for the next and previous
     $(".page").removeAttr("title");
-    $(".next").attr("title", "volgende");
-    $(".prev").attr("title", "vorige");
+    $(".next").attr("title", "Volgende");
+    $(".prev").attr("title", "Vorige");
 });
